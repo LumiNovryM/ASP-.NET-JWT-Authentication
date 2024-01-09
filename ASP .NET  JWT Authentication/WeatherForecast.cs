141742630b85e366960c6077dab4f00e4f62d409
Kenshi Takahashi <92445460+LumiNovryM@users.noreply.github.com>
2024-01-09 09:45:10 +0700

    Add project files.

---
+++ ASP .NET  JWT Authentication/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace ASP_.NET__JWT_Authentication
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
